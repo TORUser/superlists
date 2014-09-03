1dbe4546dd2cefa36a7401847b57ebbc1123bfb3
Carole Shaw <cshaw@fhcrc.org>
2014-09-03 11:33:58 -0700

    Modified

--- functional_tests.py
+++ functional_tests.py
@@ -2,20 +2,21 @@ from selenium import webdriver
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
+
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
 	def tearDown(self):
 		self.browser.quit()
 
-def test_can_start_a_list_and_retrieve_it_later(self):
-	# checkout homepage for to-do app
-	self.browser.get('http://localhost:8000')
+	def test_can_start_a_list_and_retrieve_it_later (self):
+		# checkout homepage for to-do app
+		self.browser.get('http://localhost:8000')
 
-	# notice the page title and header mention to-do lists
-	assert 'Django' in self.browser.title
-	self.assertIn('To-Do', self.browser.title)
-	#self.fail("Finish the test!")
+		# notice the page title and header mention to-do lists
+		assert 'Django' in self.browser.title
+		self.assertIn('To-Do', self.browser.title)
+		self.fail("Finish the test!")
 
 # upon invite for to-do entry, user enters a to-do item: Buy apples
 # at enter, page lists:
@@ -36,5 +37,5 @@ def test_can_start_a_list_and_retrieve_it_later(self):
 # and an invite for to-do entry
 # and a unique url for user to-do list
 
-#if __name__ == '__main__':
-#	unittest.main(warnings='ignore')
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
