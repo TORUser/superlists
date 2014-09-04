a0a7f5799428707d3a858a57df0d3e6403c33ab4
Carole Shaw <cshaw@fhcrc.org>
2014-09-04 15:46:29 -0700

    Green, added url for the-one-and-only-list-in-the-world

--- lists/tests.py
+++ lists/tests.py
@@ -5,6 +5,15 @@ from lists.views import home_page
 from django.template.loader import render_to_string
 from lists.models import Item
 
+class ListViewTest(TestCase):
+	def test_displays_all_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+		
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+		self.assertContains(response, 'itemey 1')
+		self.assertContains(response, 'itemey 2')
+
 class HomePageTest(TestCase):
 
 	def test_home_page_only_saves_items_when_necessary(self):
@@ -42,7 +51,7 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 
 		self.assertEqual(response.status_code, 302)
-		self.assertEqual(response['location'], '/')
+		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 		
 	def test_home_page_displays_all_list_items(self):
 		Item.objects.create(text='itemey 1')
