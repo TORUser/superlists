3abbc3d08231573ed7d9213880b5bcc94a0984d8
Carole Shaw <cshaw@fhcrc.org>
2014-09-04 17:01:05 -0700

    URLS for forms x2, moved code in views and tests, new URL

--- superlists/urls.py
+++ superlists/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'superlists.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+	# Examples:
+	# url(r'^$', 'superlists.views.home', name='home'),
+	# url(r'^blog/', include('blog.urls')),
 
-    # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'lists.views.home_page', name='home'),                                        # matches function in superlists/views.py
+	# url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'lists.views.home_page', name='home'),                                        # matches function in superlists/views.py
 	url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='view_list'),  
+	url(r'^lists/new$', 'lists.views.new_list', name='new_list'),  
 )
