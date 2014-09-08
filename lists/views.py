7e761c02651807582deb62294eb83e34da963927
Carole Shaw <cshaw@fhcrc.org>
2014-09-08 15:03:06 -0700

    Use Bootstrap to improve layout

--- lists/views.py
+++ lists/views.py
@@ -19,22 +19,3 @@ def new_list(request):
 	list_ = List.objects.create()
 	Item.objects.create(text=request.POST['item_text'], list=list_)
 	return redirect('/lists/%d/' % (list_.id,))
-
-# def home_page(request):
-    # if request.method == 'POST':
-        # new_item_text = request.POST['item_text']
-        # Item.objects.create(text=new_item_text)
-    # else:
-        # new_item_text = ''
-
-    # return render(request, 'home.html', {
-        # 'new_item_text': new_item_text, })
-
-# def home_page(request):
-	# item = Item()
-	# item.text = request.POST.get('item_text', '')
-	# item.save()
-
-	# return render(request, 'home.html', {
-		# 'new_item_text':  item.text
-	# })
