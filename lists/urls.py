07b7d1b1109ece15b2c50e198c1bafda1f6dfc3a
Carole Shaw <cshaw@fhcrc.org>
2014-09-08 12:28:29 -0700

    Adjusted git configuration for core.autocrlf

--- lists/urls.py
+++ lists/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 
+
 urlpatterns = patterns('',
     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
     url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
