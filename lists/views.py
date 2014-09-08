362009e02b6c0ade80fdd93af547c8a4741b7255
Carole Shaw <cshaw@fhcrc.org>
2014-09-08 11:37:53 -0700

    All tests passing

--- lists/views.py
+++ lists/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
+def add_item(request, list_id):
+	list_ = List.objects.get(id=list_id)
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect('/lists/%d/' % (list_.id,))
+
 def home_page(request):
 	return render(request, 'home.html')
 
-def view_list(request):
-	items = Item.objects.all()
-	# return HttpResponse()
+def view_list(request, list_id):
+	list_ = List.objects.get(id=list_id)
+	items = Item.objects.filter(list=list_)
 	return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-	Item.objects.create(text=request.POST['item_text'])
-	return redirect( '/lists/the-only-list-in-the-world/' )
+	list_ = List.objects.create()
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect('/lists/%d/' % (list_.id,))
 
 # def home_page(request):
     # if request.method == 'POST':
