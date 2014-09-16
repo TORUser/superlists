a982ef7bdab14a4f78b2b8b4e2dd046c1ef9f125
Carole Shaw <cshaw@fhcrc.org>
2014-09-16 11:52:49 -0700

    Removed hack in case it is causing my difficulties ...

--- superlists/wsgi.py
+++ superlists/wsgi.py
@@ -7,14 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 added import: django and django.setup() 9/12/2014 Carole
+import django
+django.setup()
 
 """
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 
-import django
-django.setup()
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
