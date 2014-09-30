237c897b95c0d67adc2a38a7104ca577c0701c98
Carole Shaw <cshaw@fhcrc.org>
2014-09-30 12:49:33 -0700

    use new form in home_page, simplify tests, breaks stuffgit status

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -3,12 +3,12 @@ from lists.forms import EMPTY_LIST_ERROR, ItemForm
 
 class ItemFormTest(TestCase):
 
-	def test_form_item_input_has_placeholder_and_css_classes(self):
-		form = ItemForm()
-		self.assertIn('placeholder="Enter a list item"', form.as_p())
-		self.assertIn('class="form-control input-lg"', form.as_p())
-		
-	def test_form_validation_for_blank_items(self):
-		form = ItemForm(data={'text': ''})
-		self.assertFalse(form.is_valid())
-		self.assertEqual( form.errors['text'], [EMPTY_LIST_ERROR] )
+    def test_form_item_input_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        self.assertIn('placeholder="Enter a list item"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+        
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual( form.errors['text'], [EMPTY_LIST_ERROR] )
